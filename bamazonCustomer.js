df53ad153f94e92261cceb01cc19df371060342e
Dewi Nurdin <dewinurdin@gmail.com>
2018-02-08 18:00:53 -0600

    added grand total and updated inventory of mySQL

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,8 @@
-  var mysql = require("mysql");
-  var inquirer = require("inquirer");
- var consoleTable = require('console-table');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('easy-table');
 
- var connection = mysql.createConnection({
+var connection = mysql.createConnection({
  	host: "localhost",
  	port: 3306,
  	user: "root",
@@ -12,50 +12,75 @@
 
  connection.connect(function(err){
  	if (err) throw err;
- 		// console.log("Connection ID: " + connection.threadId);
+ 		console.log("Connection ID: " + connection.threadId);
  });
 
  function endConnection(){
  	connection.end();
  };
 
- // Running this application will first display all of the items available for sale. 
- //Include the ids, names, and prices of products for sale.
+ var t = new Table;
+
+connection.query("SELECT * FROM products", function(err, res){
+	if (err) throw err;
+	
+	for (var i =0; i < res.length; i++){
+		t.cell('Product Id', res[i].title_id)
+		t.cell('Description', res[i].product_name)
+		t.cell('Department', res[i].department_name)
+		t.cell("Price", res[i].price)
+		t.cell("Quantity", res[i].stock_quantity)
+		t.newRow()		 
+	}
+	console.log(t.toString())
+
+	start();
+});
 
 //  The app should then prompt users with two messages.
 // The first should ask them the ID of the product they would like to buy.
 // The second message should ask how many units of the product they would like to buy.
 var prompt = inquirer.createPromptModule();
- 
-prompt([{
-		name: "purchase",
+var start = function(){
+	prompt([
+		{
+		name: "id",
 		type: "input",
 		message: "Select product ID that you would like to buy: " + "\r\n",
-		pageSize: displayItems(),		
 	}, {
 		name: "quantity",
 		type: "input",
-		message: "How many unit would you like to purchase?",
-	}
-	]).then(function(answer){
-		connection.query("SELECT * FROM products WHERE ?", {
-			purchase: answer.purchase,
-			quantity: answer.quantity
-		}, function(err, res){
-				if (err) {
-					console.log(err);
-				} else {
-					
-				}
-		})
-	});
+		message: "How many unit would you like to purchase?" + "\r\n",
+	
 
-function displayItems(){
- 			connection.query("SELECT * FROM products", function(err, res){
- 			for (var i = 0; i < res.length; i++){
-	 				consoleTable([
-					  [res[i].title_id, " | ", res[i].product_name, " | ", res[i].department_name, " | ", res[i].price, " | ", res[i].stock_quantity],
-					 ]); 		
-		 			}
-			 	})
-			 };
+	}]).then(function(answer){
+		// answer.id
+		// answer.quantity
+		connection.query("SELECT * FROM products WHERE title_id = ?", [answer.id], function(err, res){
+			if(err) throw err;
+
+		var total = parseFloat(res[0].price).toFixed(2) * parseInt(answer.quantity);
+			console.log("Your grand total is: $",total);
+
+			updateQuantity(res, answer);
+		})			
+	})		
+};
+ 
+// parameters
+
+var updateQuantity = function(res, answer) {
+			// answer.quantity
+			// res[0].stock_quantity
+
+			// Substract the user order quantity from Inventory 
+			var remaining = parseInt(res[0].stock_quantity) - parseInt(answer.quantity);
+
+			connection.query("UPDATE products SET stock_quantity = ? WHERE title_id = ?", [remaining, answer.id], function(err, quantity){
+				if(err) throw err;
+
+				console.log("Database has been updated!");
+				console.log("Current inventory for that product is ", remaining);
+			});			
+};
+
